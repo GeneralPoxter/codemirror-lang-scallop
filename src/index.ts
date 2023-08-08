e595a4ae1b64bbcca16735c2d25285d7147c3df3
GeneralPoxter <generalpoxter@gmail.com>
2023-08-08 17:28:09 -0400

    bleh

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,10 @@ export const ScallopLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
       }),
       foldNodeProp.add({
-        Application: foldInside
       }),
       styleTags({
-        Identifier: t.variableName,
-        Boolean: t.bool,
-        String: t.string,
-        LineComment: t.lineComment,
         "( )": t.paren
       })
     ]
