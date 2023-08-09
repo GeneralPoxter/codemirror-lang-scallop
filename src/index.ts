e21f7ab12e3b90fd3ed0c00ff0868b9f58cfb796
GeneralPoxter <generalpoxter@gmail.com>
2023-08-08 20:59:41 -0400

    add syntax highlighting

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,23 @@ import {styleTags, tags as t} from "@lezer/highlight"
 export const ScallopLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
-      indentNodeProp.add({
-      }),
-      foldNodeProp.add({
-      }),
       styleTags({
-        "( )": t.paren
+        LineComment: t.lineComment,
+        String: t.string,
+        Boolean: t.bool,
+        Keyword: t.keyword,
+        Var: t.variableName,
+        Identifier: t.name,
+        ArithOp: t.arithmeticOperator,
+        LogicOp: t.logicOperator,
+        CompareOp: t.compareOperator,
+        "( )": t.paren,
+        "{ }": t.brace,
       })
     ]
   }),
   languageData: {
-    commentTokens: {line: ";"}
+    commentTokens: {line: "//"}
   }
 })
 
